375cb1b20406ae48f1fcf605d4fb5050de97fed3
Marcel du Preez <marceldupr@gmail.com>
2012-09-17 13:32:13 +0100

    fixed issue with routes

--- routes/site.rb
+++ routes/site.rb
@@ -1,3 +1,41 @@
+get '/console' do
+  if(logged_in?)
+    content_type :html
+    erb :'koda/console', :layout => false, :escape_html => true
+  else
+    session['return_url'] = '/console'
+    redirect '/sign-in'
+  end
+end
+
+get '/explorer' do
+  if(logged_in?)
+    content_type :html
+    erb :'koda/explorer', :layout => false, :escape_html => true
+  else
+    session['return_url'] = '/explorer'
+    redirect '/sign-in'
+  end
+end
+
+get "/sign-in" do
+  content_type :html
+  erb :'koda/login', :layout => false, :escape_html => true
+end
+
+get "/not-allowed" do
+  content_type :html
+  erb :'koda/not_allowed', :layout => false, :escape_html => true
+end
+
+post "/signed-in" do
+  if authenticate(params[:token])
+    puts session['return_url']
+    redirect session['return_url']
+  else
+    redirect "/sign-in"
+  end
+end
 
 get '/' do
   content_type :html
@@ -6,5 +44,5 @@ end
 
 get '/:page?' do
   content_type :html
-  erb params[:page], :escape_html => true
+  erb :generic, :escape_html => true
 end
